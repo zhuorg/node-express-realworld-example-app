547d009a326d11079ebf3b191842169bf37454d7
zhu5201314 <46183853+zhu5201314@users.noreply.github.com>
2020-04-28 18:23:40 +0800

    Update hello.js

--- hello.js
+++ hello.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(8000, 'APP_PRIVATE_IP_ADDRESS');
-console.log('Server running at http://APP_PRIVATE_IP_ADDRESS:8000/');
+}).listen(8000, 'localhost');
+console.log('Server running at http://localhost:8000/');
